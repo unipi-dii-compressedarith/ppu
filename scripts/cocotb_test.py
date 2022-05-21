dd0e0a10a0e7ff6d1c00e70da8802b04e799e34a
federico <f.rossi57@studenti.unipi.it>
2022-05-21 19:54:52 +0200

    Added initial setup for cocotb tests

---
+++ scripts/cocotb_test.py
@@ -0,0 +1,16 @@
+# test_dff.py
+
+import random
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import FallingEdge
+
+@cocotb.test()
+async def test_dff_simple(dut):
+    """ Test that d propagates to q """
+
+    clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
+    cocotb.start_soon(clock.start())  # Start the clock
+
+    for i in range(10):
+        await FallingEdge(dut.clk)
