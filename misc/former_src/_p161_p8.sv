a15cdbf6fa4fe9d5c6f862df1f93177e78b52a68
federico <f.rossi57@studenti.unipi.it>
2022-06-22 10:12:56 +0200

    Cleanup

--- misc/former_src/_p161_p8.sv
+++
@@ -1,7 +0,0 @@
-module p161_p8(p161,p8);
-	input logic[15:0] p161;
-    output logic signed[7:0] p8;
-	always @(*) begin
-		p8 = p161 >> 8;
-	end
-endmodule
--- misc/former_src/_p8_p161.sv
+++
@@ -1,8 +0,0 @@
-module p8_p161(p8,p161);
-	input logic signed[7:0] p8;
-	output logic[15:0] p161;
-
-	always @(*) begin
-		p161 = {p8,8'b0000000};
-	end
-endmodule         
